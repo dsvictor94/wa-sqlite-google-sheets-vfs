7084a1311787278311faab7dd5fd489ebd5233c0
Victor Duarte <ds.victor94@gmail.com>
2026-06-19 22:30:35 -0400

    feat: export public API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { GoogleBrowserAuth, SHEETS_DISCOVERY_DOC, SHEETS_SCOPE } from "./google-browser-auth.js";
+export { GoogleSdkSheetsClient } from "./google-sheets-client.js";
+export { GoogleSheetsSQLiteVFS } from "./google-sheets-vfs.js";
+export { ensureGoogleSheetsVfsTabs } from "./sheets-setup.js";
+export type { GoogleBrowserSheetsConfig, GoogleSheetsVFSOptions } from "./types.js";
