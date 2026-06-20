a81fef277d395d41bb260bb658d2e02c70706bd6
Victor Duarte <ds.victor94@gmail.com>
2026-06-19 23:29:30 -0400

    feat: export spreadsheet helper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { GoogleBrowserAuth, SHEETS_DISCOVERY_DOC, SHEETS_SCOPE } from "./google-browser-auth.js";
-export { GoogleSdkSheetsClient } from "./google-sheets-client.js";
+export { createGoogleSheetsVfsSpreadsheet, GoogleSdkSheetsClient } from "./google-sheets-client.js";
 export { GoogleSheetsSQLiteVFS } from "./google-sheets-vfs.js";
 export { ensureGoogleSheetsVfsTabs } from "./sheets-setup.js";
-export type { GoogleBrowserSheetsConfig, GoogleSheetsVFSOptions } from "./types.js";
+export type { CreatedSpreadsheet, GoogleBrowserSheetsConfig, GoogleSheetsVFSOptions } from "./types.js";
