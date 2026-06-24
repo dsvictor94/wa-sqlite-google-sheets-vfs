6e79c746ca1fbdd42bdc6fc95b230e1a0ccfd294
Victor Duarte <ds.victor94@gmail.com>
2026-06-24 19:10:45 -0300

    Expand state helper shape

--- src/sheets-state.ts
+++ src/sheets-state.ts
@@ -1,4 +1,4 @@
-import { GoogleSdkSheetsClient, type SpreadsheetRequest } from "./google-sheets-client.js";
+import { GoogleSdkSheetsClient, type SpreadsheetBatchUpdateResult, type SpreadsheetRequest } from "./google-sheets-client.js";
 
 export type GoogleSheetsLeaseOptions = { databaseId: string; blockSheetName?: string; leaseMs?: number; lockTimeoutMs?: number; lockReleaseDelayMs?: number };
 export type GoogleSheetsWriteBatchRenewal = { requests: SpreadsheetRequest[]; replyIndex: number; expiresAtSec: string };
@@ -6,4 +6,5 @@ export type GoogleSheetsWriteBatchRenewal = { requests: SpreadsheetRequest[]; re
 export class GoogleSheetsLease {
   constructor(private readonly client: GoogleSdkSheetsClient, options: GoogleSheetsLeaseOptions) { void this.client; void options; }
   async acquire(): Promise<boolean> { return true; }
+  completeWriteBatchRenewal(_response: SpreadsheetBatchUpdateResult, _renewal: GoogleSheetsWriteBatchRenewal): void {}
 }
