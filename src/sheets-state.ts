7a41d1cd2550a3cdb0e8f3084cb64c007721cefa
Victor Duarte <ds.victor94@gmail.com>
2026-06-24 18:55:45 -0300

    Small state update

--- src/sheets-state.ts
+++ src/sheets-state.ts
@@ -1,7 +1,9 @@
-import { type SpreadsheetRequest } from "./google-sheets-client.js";
+import { GoogleSdkSheetsClient, type SpreadsheetRequest } from "./google-sheets-client.js";
 
+export type GoogleSheetsLeaseOptions = { databaseId: string; blockSheetName?: string; leaseMs?: number; lockTimeoutMs?: number; lockReleaseDelayMs?: number };
 export type GoogleSheetsWriteBatchRenewal = { requests: SpreadsheetRequest[]; replyIndex: number; expiresAtSec: string };
 
 export class GoogleSheetsLease {
+  constructor(private readonly client: GoogleSdkSheetsClient, options: GoogleSheetsLeaseOptions) { void this.client; void options; }
   async acquire(): Promise<boolean> { return true; }
 }
