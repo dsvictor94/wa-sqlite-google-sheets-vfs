104a1e427d010c303c71ee2a173238362637a152
Victor Duarte <ds.victor94@gmail.com>
2026-06-23 21:50:07 -0300

    Add wa-sqlite sqlite-constants typings

---
+++ src/wa-sqlite-sqlite-constants.d.ts
@@ -0,0 +1,9 @@
+declare module "wa-sqlite/src/sqlite-constants.js" {
+  export const SQLITE_IOERR: number;
+  export const SQLITE_IOERR_COMMIT_ATOMIC: number;
+  export const SQLITE_NOTFOUND: number;
+  export const SQLITE_FCNTL_BEGIN_ATOMIC_WRITE: number;
+  export const SQLITE_FCNTL_COMMIT_ATOMIC_WRITE: number;
+  export const SQLITE_FCNTL_ROLLBACK_ATOMIC_WRITE: number;
+  export const SQLITE_IOCAP_BATCH_ATOMIC: number;
+}
