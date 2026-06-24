3b517cc56d7f919c1216bf9b3cc7b5eede984b07
Victor Duarte <ds.victor94@gmail.com>
2026-06-24 18:55:35 -0300

    Small state update

--- src/sheets-state.ts
+++ src/sheets-state.ts
@@ -1,3 +1,7 @@
+import { type SpreadsheetRequest } from "./google-sheets-client.js";
+
+export type GoogleSheetsWriteBatchRenewal = { requests: SpreadsheetRequest[]; replyIndex: number; expiresAtSec: string };
+
 export class GoogleSheetsLease {
   async acquire(): Promise<boolean> { return true; }
 }
