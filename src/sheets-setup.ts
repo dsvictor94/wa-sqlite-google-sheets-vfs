fa91bfbb622d95f55966af92c63989283b3bae0c
Victor Duarte <ds.victor94@gmail.com>
2026-06-25 21:00:26 -0300

    feat: allow setup helper metrics collection

--- src/sheets-setup.ts
+++ src/sheets-setup.ts
@@ -1,9 +1,10 @@
 import { BLOCK_SHEET_INITIAL_COLUMNS, BLOCK_SHEET_INITIAL_ROWS, DEFAULT_BLOCK_SHEET_NAME, LOCK_CELL_PREFIX, LOCK_STATE_CELL } from "./constants.js";
 import { GoogleSdkSheetsClient } from "./google-sheets-client.js";
+import type { GoogleSheetsVFSMetrics } from "./types.js";
 import { quoteSheetName } from "./util.js";
 
-export async function ensureGoogleSheetsVfsTabs(spreadsheetId: string): Promise<void> {
-  const client = new GoogleSdkSheetsClient(spreadsheetId);
+export async function ensureGoogleSheetsVfsTabs(spreadsheetId: string, metrics?: GoogleSheetsVFSMetrics): Promise<void> {
+  const client = new GoogleSdkSheetsClient(spreadsheetId, metrics);
   const sheet = quoteSheetName(DEFAULT_BLOCK_SHEET_NAME);
 
   await client.ensureSheetTabs([
