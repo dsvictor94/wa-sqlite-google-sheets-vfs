264ee307635a5ffcc7a41b94521e0db499864a8b
Victor Duarte <ds.victor94@gmail.com>
2026-06-25 22:03:00 -0300

    Implement Sheets VFS recovery protocol

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,12 @@
 export const GOOGLE_SHEETS_BLOCK_BYTES = 4096 as const;
-export const DEFAULT_BLOCK_SHEET_NAME = "__sqlite_blocks";
-export const DEFAULT_LOCK_SHEET_NAME = "__sqlite_blocks";
+
+export const CONTROL_SHEET_NAME = "Control";
+export const DATA_SHEET_NAME = "Data";
+export const CONTROL_SHEET_ID = 100000;
+export const INITIAL_DATA_SHEET_ID = 100001;
+
+export const DEFAULT_BLOCK_SHEET_NAME = DATA_SHEET_NAME;
+export const DEFAULT_LOCK_SHEET_NAME = CONTROL_SHEET_NAME;
 export const DEFAULT_BLOCKS_PER_STRIPE = 256;
 export const DEFAULT_STRIPES_PER_FILE = 1024;
 export const DEFAULT_LEASE_MS = 30_000;
@@ -8,7 +14,7 @@ export const DEFAULT_LOCK_TIMEOUT_MS = 10_000;
 export const DEFAULT_LOCK_RELEASE_DELAY_MS = 1_000;
 export const DEFAULT_CACHE_BLOCKS = 128;
 
-export const LOCK_CELL_PREFIX = "LSV1|";
+export const LOCK_CELL_PREFIX = "LSV2|";
 export const LOCK_HEADER_RANGE = "A1:H1";
 export const LOCK_STATE_CELL = "A1";
 export const LOCK_STATE_ROW_INDEX = 0;
