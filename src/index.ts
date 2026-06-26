b888f96b4fc4a7122560f40d489a1123acd9be13
Victor Duarte <ds.victor94@gmail.com>
2026-06-26 11:43:52 -0300

    Export VFS from original module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { DEFAULT_GOOGLE_SCOPES, DRIVE_FILE_SCOPE, GoogleBrowserAuth, SHEETS_DISCOVERY_DOC, SHEETS_SCOPE } from "./google-browser-auth.js";
 export { createGoogleSheetsVfsSpreadsheet, GoogleSdkSheetsClient } from "./google-sheets-client.js";
-export { GoogleSheetsSQLiteVFS } from "./google-sheets-vfs-recovery.js";
+export { GoogleSheetsSQLiteVFS } from "./google-sheets-vfs.js";
 export { GoogleSheetsLease } from "./sheets-state.js";
 export { ensureGoogleSheetsVfsTabs } from "./sheets-setup.js";
 export type { GoogleSheetsLeaseOptions, GoogleSheetsWriteBatchRenewal } from "./sheets-state.js";
